17ea3f200ce1ac74970a870fb6f788827b05cbc4
jidoggs <jidoggs@gmail.com>
2022-04-17 20:59:54 +0100

    Cannot destructure property 'data' of allShoes fixed

--- src/component/listings/ListSelector.js
+++ src/component/listings/ListSelector.js
@@ -3,30 +3,16 @@ import { useSelector } from "react-redux";
 import { getCategoryName, resultFilter } from "../../helpers";
 import ItemCard from "../ItemCard";
 
-function ListSelector({ pathname,type }) {
+function ListSelector({ pathname, type }) {
   const allShoes = useSelector((state) => state.networkRequestReducer);
-
-  const {data , minMax} = allShoes[getCategoryName(pathname)];
-
   const savedItems = useSelector((state) => state.savedReducer.savedItems);
-
+  
   let template = null;
-
+  
   switch (type) {
     case "LIST_ITEMS":
-      template = resultFilter(data, minMax?.filter, allShoes.brandsFilter).map((itm, id) => (
-          <ItemCard
-            key={id}
-            title={itm.title}
-            price={itm.retailPrice}
-            id={itm.id}
-            image={itm.media.thumbUrl}
-          />
-        ));
-      break;
-    
-    case "SAVED_ITEMS":
-      template = (savedItems.map(
+      const { data, minMax } = allShoes[getCategoryName(pathname)];
+      template = resultFilter(data, minMax?.filter, allShoes.brandsFilter).map(
         (itm, id) => (
           <ItemCard
             key={id}
@@ -36,6 +22,18 @@ function ListSelector({ pathname,type }) {
             image={itm.media.thumbUrl}
           />
         )
+      );
+      break;
+
+    case "SAVED_ITEMS":
+      template = savedItems.map((itm, id) => (
+        <ItemCard
+          key={id}
+          title={itm.title}
+          price={itm.retailPrice}
+          id={itm.id}
+          image={itm.media.thumbUrl}
+        />
       ));
       break;
 
