e6d0bf7d9fe15c41bdfd3117aa3f7f231a9e05ad
jidoggs <jidoggs@gmail.com>
2022-04-28 16:57:23 +0100

    details page only make an api call when nessesary

--- src/component/product/Product.js
+++ src/component/product/Product.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import Loading from "../HOC/Loading";
@@ -12,42 +13,61 @@ const ProductStyle = styled.div`
   display: flex;
   column-gap: 4rem;
 
-  @media(max-width:954px){
+  @media (max-width: 954px) {
     grid-column: 2/-2;
-    flex-direction: column;
+    flex-direction: ${(props) => (props.pid ? "column" : "row")};
   }
 `;
 
 function Product() {
   const { id } = useParams();
   const [data, setData] = useState({});
+  const cart = useSelector((state) =>
+    state.cartReducer.cart.filter((cartItm) => cartItm.id === id)
+  );
+  const saved = useSelector((state) =>
+    state.savedReducer.savedItems.filter((savedItm) => savedItm.id === id)
+  );
+
+  const cartLength = useMemo(() => cart.length,[cart.length]) //eslint-disable-line
+
+  const savedLength = useMemo(() => saved.length, [saved.length]) //eslint-disable-line
 
   useEffect(() => {
+    if (cartLength === 1) {
+      setData(cart[0]);
+    }
+    if (savedLength === 1) {
+      setData(saved[0]);
+    }
+    
+    if (cartLength !== 1 && savedLength !== 1) {
+    console.log("fetched")
     const options = {
-      method: "GET",
-      url: `https://v1-sneakers.p.rapidapi.com/v1/sneakers/${id}`,
-      headers: {
-        "X-RapidAPI-Host": "v1-sneakers.p.rapidapi.com",
-        "X-RapidAPI-Key": 
-        // "25fc60c808msh221fbd0ae3cb637p1e48c2jsndcedce7a559e",
-        // "6a9e1ed7d0mshad15c22d4814a43p1aa588jsnb5eab34da227",
-        "c08c020cb4msh1cac15acce21030p1084f0jsna581e807075f",
-      },
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        setData(response.data?.results[0]);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }, [id]);
+        method: "GET",
+        url: `https://v1-sneakers.p.rapidapi.com/v1/sneakers/${id}`,
+        headers: {
+          "X-RapidAPI-Host": "v1-sneakers.p.rapidapi.com",
+          "X-RapidAPI-Key":
+            // "25fc60c808msh221fbd0ae3cb637p1e48c2jsndcedce7a559e",
+            // "6a9e1ed7d0mshad15c22d4814a43p1aa588jsnb5eab34da227",
+            "c08c020cb4msh1cac15acce21030p1084f0jsna581e807075f",
+        },
+      };
+      axios
+        .request(options)
+        .then(function (response) {
+          setData(response.data?.results[0]);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  }, [cartLength, savedLength]);//eslint-disable-line
 
   return (
-    <ProductStyle>
-      {data.id  ? (
+    <ProductStyle pid={data.id}>
+      {data.id ? (
         <>
           <ProductCanvas data={data} />
           <ProductDesc data={data} />
